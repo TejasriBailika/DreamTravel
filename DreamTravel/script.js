0409db705c1e9c39baf6db8f683b2b241ed2348a
TejasriBailika <137523449+TejasriBailika@users.noreply.github.com>
2025-01-19 17:43:34 +0530

    Update script.js

--- DreamTravel/script.js
+++ DreamTravel/script.js
@@ -5,9 +5,8 @@ document.getElementById('sign-in').addEventListener('click', function() {
 document.getElementById('sign-up').addEventListener('click', function() {
     window.open('sign-up.html', '_blank');
   });
-//Function to open a new page when a floating label is clicked
 function openPage(page) {
-  window.location.href = page; // Redirect to the specified page
+  window.location.href = page; 
 }
 
 
