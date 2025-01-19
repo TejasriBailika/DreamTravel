e1c68d0b976eaf2c78598eddea9e14a3dd5a2689
TejasriBailika <137523449+TejasriBailika@users.noreply.github.com>
2025-01-19 17:44:13 +0530

    Update signup.js

--- DreamTravel/signup.js
+++ DreamTravel/signup.js
@@ -1,23 +1,20 @@
 document.getElementById("sign-up-form").addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent form from submitting the traditional way
+  event.preventDefault(); 
 
-  // Get user input values
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   const confirmPassword = document.getElementById("confirm-password").value;
 
-  // Check if password and confirm password match
   if (password !== confirmPassword) {
     alert("Passwords do not match.");
     return;
   }
 
-  // Store the user's data in local storage
   localStorage.setItem("email", email);
   localStorage.setItem("password", password);
 
   alert("Sign-Up successful!");
-  window.location.href = "sign-in.html"; // Redirect to sign-in page after sign-up
+  window.location.href = "sign-in.html";
 });
 
 
